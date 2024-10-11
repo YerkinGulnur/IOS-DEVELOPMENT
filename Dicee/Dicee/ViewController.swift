be5a2e0e8d145ac2f7e9afc7de18da70ad447cc5
YerkinGulnur <157001820+YerkinGulnur@users.noreply.github.com>
2024-10-11 12:44:06 +0500

    Add files via upload

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,19 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var DiceImageViewOne: UIImageView!
+    @IBOutlet weak var DiceImageViewTwo: UIImageView!
+
+    let diceArray = [ #imageLiteral(resourceName: "dice1"),#imageLiteral(resourceName: "dice2"),#imageLiteral(resourceName: "dice3"),#imageLiteral(resourceName: "dice4"),#imageLiteral(resourceName: "dice5"),#imageLiteral(resourceName: "dice6")]
+    
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        DiceImageViewOne.image = diceArray.randomElement()
+        DiceImageViewTwo.image = diceArray.randomElement()
+    }
+    
+}
+
