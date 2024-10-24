339d60431f3a69bd0ab36ddd4c18359bcbfe8547
YerkinGulnur <157001820+YerkinGulnur@users.noreply.github.com>
2024-10-24 16:26:52 +0500

    Update ViewController.swift

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -9,63 +9,63 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     
     let eggTimes: [String: Int] = ["Soft": 3, "Medium": 5, "Hard": 7]
+    var totalTime = 0
+    var secondsPassed = 0
+    var timer = Timer()
+    var player: AVAudioPlayer!
     
-    private var  player: AVAudioPlayer?
-    private var timer = Timer()
-    private var totalTime = 0
-    private var secondsPassed = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-   
-    @IBAction func sofEggTapped(_ sender: UITapGestureRecognizer) {
-        startTimer(eggType: "Soft")
-    }
-    @IBAction func mediumEggTapped(_ sender: UITapGestureRecognizer) {
-        startTimer(eggType: "Medium")
-    }
-    @IBAction func hardEggTapped(_ sender: UITapGestureRecognizer) {
-        startTimer(eggType: "Hard")
-    }
     
-    func startTimer(eggType: String) {
-            timer.invalidate()
-            
-            totalTime = eggTimes[eggType]!
-            progressBar.progress = 0.0
-            secondsPassed = 0
-            titleLabel.text = eggType
-            
-            //запуск нового таймера
-            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    @IBAction func eggButtonPressed(_ sender: UIButton) {
+        timer.invalidate()
+        
+        guard
+            let eggTitle = sender.title(for: .normal),
+            let overallTime = eggTimes[eggTitle]
+        else {
+            return
         }
-    
+        
+        totalTime = overallTime
+        secondsPassed = 0
+        progressBar.progress = 0.0
+        titleLabel.text = "\(totalTime) seconds"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
     @objc func updateTimer() {
-           if secondsPassed < totalTime {
-               secondsPassed += 1
-               progressBar.progress = Float(secondsPassed) / Float(totalTime)
-           } else {
-               timer.invalidate()
-               titleLabel.text = "DONE"
-               playSound()
-           }
-       }
-    
-    func playSound() {
-            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
-        player = try! AVAudioPlayer(contentsOf: url!)
-            player?.play()
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            titleLabel.text = "\(totalTime - secondsPassed) seconds"
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+            playSound()
         }
-    
-    
-    
-    
+    }
+    func playSound() {
+        guard
+            let soundUrl = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        else {
+            return
+        }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: soundUrl)
+                  player?.play()
+        } catch {
+            print("Error playing sound: \(error)")
+        }
+    }
 }
-
