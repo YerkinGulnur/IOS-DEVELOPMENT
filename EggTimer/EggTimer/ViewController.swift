9ef0b4b0d19e37035ed0d5a49d51c95be15bfbc1
YerkinGulnur <157001820+YerkinGulnur@users.noreply.github.com>
2024-10-25 18:07:54 +0500

    Update ViewController.swift

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -9,63 +9,58 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    let eggTimes: [String: Int] = ["Soft": 3, "Medium": 4, "Hard": 5]
     
-    
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var progressBar: UIProgressView!
-    
-    let eggTimes: [String: Int] = ["Soft": 3, "Medium": 5, "Hard": 7]
-    var totalTime = 0
-    var secondsPassed = 0
-    var timer = Timer()
-    var player: AVAudioPlayer!
-    
-    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var progressBar: UIProgressView!
+    private var player: AVPlayer?
+    private var timer: Timer?
+    private var timeCounter: Int = 0
+    private var overallTime: Int = 0
+    @IBOutlet weak var softEggButton: UIButton!
+    @IBOutlet weak var mediumEggButton: UIButton!
+    @IBOutlet weak var hardEggButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        titleLabel.text = "How do you like your eggs?"
+        progressBar.progress = 0.0
+        
+        softEggButton.setTitle("Soft", for: .normal)
+        mediumEggButton.setTitle("Medium", for: .normal)
+        hardEggButton.setTitle("Hard", for: .normal)
     }
     
-    @IBAction func eggButtonPressed(_ sender: UIButton) {
-        timer.invalidate()
-        
-        guard
-            let eggTitle = sender.title(for: .normal),
-            let overallTime = eggTimes[eggTitle]
-        else {
+    @IBAction func eggButtonDidTap(_ sender: UIButton) {
+        guard let eggTitle = sender.title(for: .normal),
+              let overallTime = eggTimes[eggTitle] else {
             return
         }
         
-        totalTime = overallTime
-        secondsPassed = 0
+        timeCounter = 0
+        timer?.invalidate()
+        titleLabel.text = eggTitle
         progressBar.progress = 0.0
-        titleLabel.text = "\(totalTime) seconds"
         
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { timer in
+            self.timeCounter += 1
+            DispatchQueue.main.async {
+                self.progressBar.progress = Float(self.timeCounter) / Float(overallTime)
+            }
+
+            if self.timeCounter == overallTime {
+                timer.invalidate()
+                DispatchQueue.main.async {
+                    self.titleLabel.text = "Egg is ready!"
+                }
+                self.playAlarmSound()
+            }
+        })
     }
-    @objc func updateTimer() {
-        if secondsPassed < totalTime {
-            secondsPassed += 1
-            progressBar.progress = Float(secondsPassed) / Float(totalTime)
-            titleLabel.text = "\(totalTime - secondsPassed) seconds"
-        } else {
-            timer.invalidate()
-            titleLabel.text = "DONE!"
-            playSound()
-        }
-    }
-    func playSound() {
-        guard
-            let soundUrl = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
-        else {
-            return
-        }
-        
-        do {
-            player = try AVAudioPlayer(contentsOf: soundUrl)
-                  player?.play()
-        } catch {
-            print("Error playing sound: \(error)")
-        }
+
+    private func playAlarmSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+        player = AVPlayer(url: url)
+        player?.play()
     }
 }
